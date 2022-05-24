9a0cea0248dd6383c8e54edbe40fb690e7d164e9
Paweł Greszta <pawel@greszta.pl>
2022-05-24 20:24:41 +0200

    Add secound part of homework ;)

--- zadanie.py
+++ zadanie.py
@@ -21,3 +21,16 @@ for shop in my_shopping:
 
 count = len(counting_products)
 print(f"W sumie kupiłem {count} produktów... \n")
+
+print("""
+Uwaga!!! tu wyświetlają się liczby
+podzielne przez 5
+z zakresu od 0 do 100:
+""")
+divide_numbers = []
+
+for n in range (0,101):
+    if n > 0 and n % 5 == 0:
+        divide_numbers.append(n)
+print(divide_numbers)
+print()
