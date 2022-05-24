95e0f6b69201bd400416a09cbbcff4aeb090671a
Paweł Greszta <pawel@greszta.pl>
2022-05-24 20:17:52 +0200

    Add dictionary my_shopping  and empty list counting_products

---
+++ zadanie.py
@@ -0,0 +1,7 @@
+my_shopping = {
+    'warzywniak': ['kapusta', 'marchew', 'buraki', 'czosnek' ],
+    'piekarnia': ['chleb', 'bułka', 'bułka', 'bagietka'],
+    'obowniczy': ['sandały'],
+    'instalacyjny': ['rurka', 'kran', 'zawór']
+    }
+counting_products = []
