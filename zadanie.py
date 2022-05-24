26d3da3dd5e3385faf3470da62e1bab52a2cacd2
Paweł Greszta <pawel@greszta.pl>
2022-05-24 20:23:00 +0200

    Add loops to print shops and products

--- zadanie.py
+++ zadanie.py
@@ -5,3 +5,19 @@ my_shopping = {
     'instalacyjny': ['rurka', 'kran', 'zawór']
     }
 counting_products = []
+
+print("""
+Wybrałem się na zakupy
+i nabyłem natępujące rzeczy
+w nastepujacych sklepach:
+""")
+
+for shop in my_shopping:
+    print(f"{shop.capitalize()}:")
+    for things in my_shopping[shop]:
+        print(f"  -{things.capitalize()}")
+        counting_products.append(things)
+    print()
+
+count = len(counting_products)
+print(f"W sumie kupiłem {count} produktów... \n")
